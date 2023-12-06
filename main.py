0e48768420b3f86b33224891042385752c6448b3
Kishore Iyer <kish0620@Kishores-MacBook-Pro-2.local>
2023-12-06 16:04:16 -0600

    added app.run and client config

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main_page():
 def profile_page():
     # client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAACFerAEAAAAAGZ1TtfAH%2BnvkvR%2Fo4Sp0dGBFvTo%3DdZrPu6tRB4gH70qMYaw4tvWWNfVrX0V5hH1LPKoZpJH2a9yeaD")
     client = tweepy.Client(
-        consumer_key="API/oK5hQ5rZn6hvNqR6c1QwD8Xe5",
-        consumer_secret="API/fhG11tIKBs2wL1NynfoO5dgJUTZVQiOtZ1smjTXzuwBCpSjaBQ",
-        access_token="1428502636379181061-oKVo2k0yOD8w4LVSs7gZpeA3w5Xrwi",
-        access_token_secret="HP6490J7ktBXTb1fH7xafhZhlWlt2VjZnbYXMBw2edOC1"
+        consumer_key="API/kSXIgzEy4k18JChvTHVEEioBQ",
+        consumer_secret="API/qOsOMGIcccIeKKINKu9nAVzGeJ8rdj4Be2gP4A85iQOEgLTQZE",
+        access_token="1428502636379181061-OS1j4TfmzTVZvue5eP32k207GOHWeL",
+        access_token_secret="I3lfHDaEsClowmwkUIK29Zg1WWRFiwOdDsj8gRGfW2bDh"
     )
     # print(client.consumer_key)
-    public_tweets = client.search_all_tweets()
+    public_tweets = client.get_blocked()
     tweets = []
     for tweet in public_tweets:
         tweets.append(tweet.text)
