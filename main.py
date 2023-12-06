46738c7ada6270445485e324fc26322086d5415b
Nabdeep Shrestha <nabdeep2@illinois.edu>
2023-12-06 08:51:21 -0500

    started trying to connect to twitter api

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import tweepy
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
+@app.route('/profile/')
+def profile_page():
+    client = tweepy.Client("H5QGYLFwtmrezKTXKvIBER8UE")
+    public_tweets = client.get_home_timeline()
+    tweets = []
+    for tweet in public_tweets:
+        tweets.append(tweet.text)
+    return jsonify(tweets)
