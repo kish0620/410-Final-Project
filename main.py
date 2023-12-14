e6bda8c3586a15f16977ecca8fb25a07ff11eccb
Kishore Iyer <kish0620@Kishores-MacBook-Pro-2.local>
2023-12-14 11:27:21 -0600

    added subjective/objective chart

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ def reddit_page():
 
     # return jsonify(posts)
     # TODO: create bar charts using the two dicts
-    return render_template('index.html', objective_subjective_dict=objective_subjective_dict)
+    # return render_template('index.html', dict_keys=list(objective_subjective_dict.keys()), vals = list(objective_subjective_dict.values()))
+    return render_template('index.html', sentiment_dict = sentiment_dict, objective_subjective_dict = objective_subjective_dict)
     # # Downloading imdb top 250 movie's data
     # url = 'http://www.imdb.com/chart/top'
     # response = requests.get(url)
